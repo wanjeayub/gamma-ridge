729c0f94672240cdb7326645bc4c8b91448520ba
Ayub Wanje <ayub.wanje@gmail.com>
2024-09-20 22:38:25 +0300

    resolve for absolute path after building

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ var __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(process.cwd(), "client", "dist", "index.html"));
 });
 
 // Routes
